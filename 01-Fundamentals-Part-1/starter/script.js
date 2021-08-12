c216681089d0e01fceb22b0eb5995fd445d63f44
crankynoah <nmickus@crankyape.com>
2021-08-12 11:21:41 -0500

    up through datatypes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,47 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas');
+// console.log(23);
+// let firstName = "Matilda";
+// let PI = 3.1415;
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// //front but
+// let country = "USA";
+// let continet = "North America";
+// let population = 331;
+// console.log(country);
+// console.log(continet);
+// console.log(population);
+// console.log(true);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 432);
+// console.log(typeof "helo");
+
+// javascriptIsFun = "Yes";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// year = 1998;
+// console.log(typeof year);
+// console.log(typeof null);
+
+let age = 30;
+
+age = 31;
+
+const birthYear = 1991
+var job = "programmer";
+job = "Teacher";
+
+lastName = "mickuse";
+console.log(lastName);
